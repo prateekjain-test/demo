7792d78b6a3c91596d6ebe0fe3ca7d2ab0563b4e
prateekjain-test <rajsinha98761@gmail.com>
2020-05-29 15:03:12 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,12 +3,37 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+	//Unused field
+        private int abc;
+
+        private String ip = "127.0.0.1";
+
 	@GetMapping("/hello")
 	public String hello() {
+		test1();
 		return "hello";
 	}
+
+    	public void test1() {
+
+        	String[] field = {"a", "b", "c", "s", "e"};
+
+        	String s = "";
+        	for (int i = 0; i < field.length; ++i) {
+            		s = s + field[i];
+        	}
+
+        	System.out.println(ip);
+
+    	}
+
+    public Boolean shouldBeWarning() {
+        return null;
+    }
 }
