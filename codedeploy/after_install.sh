7be2581b61009148058f39063b919b5d48a055f5
prateekjain-test <rajsinha98761@gmail.com>
2020-06-03 20:41:56 +0530

    Delete after_install.sh

--- codedeploy/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-parentdir="$(dirname "$DIR")"
-sudo chown -R ubuntu:ubuntu /home/ubuntu/testcodedeploy
-demopid=$(pidof java)
-kill -9 $demopid
-rm -rf /home/ubuntu/testcodedeploy/revision.txt
-cp $parentdir/target/demo-0.0.1-SNAPSHOT.jar /tmp/demo-0.0.1-SNAPSHOT.jar
-java -jar /tmp/demo-0.0.1-SNAPSHOT.jar > /tmp/logs.txt  2>&1 &
